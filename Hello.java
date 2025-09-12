ea16fe26391c42577bd0cdb56591e691b77d9311
olegnapass-creator <olegnapass@gmail.com>
2025-09-12 18:35:32 +0300

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,4 +2,52 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
-}
+}java -Xmx1500M -Xms1500M \
+-javaagent:authlib-injector-1.2.5.jar=https://authserver.ely.by \
+-jar start.jar nogui
+
+    java -Xmx1500M -Xms1500M \
+-javaagent:authlib-injector-1.2.5.jar=https://authserver.ely.by \
+-jar start.jar nogui
+
+    java -Xmx1500M -Xms1500M \
+-javaagent:authlib-injector-1.2.5.jar=https://authserver.ely.by \
+-jar start.jar nogui
+
+    java -Xmx1500M -Xms1500M \
+-javaagent:authlib-injector-1.2.5.jar=https://authserver.ely.by \
+-jar start.jar nogui
+
+    java -Xmx1500M -Xms1500M \
+-javaagent:authlib-injector-1.2.5.jar=https://authserver.ely.by \
+-jar start.jar nogui
+
+    java -Xmx1500M -Xms1500M \
+-javaagent:authlib-injector-1.2.5.jar=https://authserver.ely.by \
+-jar start.jar nogui
+
+    java -Xmx1500M -Xms1500M \
+-javaagent:authlib-injector-1.2.5.jar=https://authserver.ely.by \
+-jar start.jar nogui
+
+    java -Xmx1500M -Xms1500M \
+-javaagent:authlib-injector-1.2.5.jar=https://authserver.ely.by \
+-jar start.jar nogui
+
+    localhosttruejava -Xmx1500M -Xms1500M \
+-javaagent:authlib-injector-1.2.5.jar=https://authserver.ely.by \
+-jar start.jar nogui
+
+8374846683748466java -Xmx1500M -Xms1500M \
+-javaagent:authlib-injector-1.2.5.jar=https://authserver.ely.by \
+-jar start.jar nogui
+
+    83748466java -Xmx1500M -Xms1500M \
+-javaagent:authlib-injector-1.2.5.jar=https://authserver.ely.by \
+-jar start.jar nogui
+
+    83748466java -Xmx1500M -Xms1500M \
+-javaagent:authlib-injector-1.2.5.jar=https://authserver.ely.by \
+-jar start.jar nogui
+
+    Chicken Gun Это Моя Жизньproot-distro login ubuntuproot-distro login ubuntuСкебобproot-distro login ubuntuСкебобproot-distro login ubuntuproot-distro login ubuntuChicken Gun Это Моя Жизнь1-7545-6860-6402-0064-297Chicken Gun Это Моя Жизнь
